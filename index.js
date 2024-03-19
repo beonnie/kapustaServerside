e2dd0199c0532c9167146de337c9cc8ede70e46a
beonnie <denis.shimonov@mail.ru>
2024-03-19 08:56:47 +0300

    added automatic data updating

--- index.js
+++ index.js
@@ -12,6 +12,12 @@ app.get('/kp/v1/getLend', (req, res)=> {
     res.download(stringInject(config.get('excelPath'), [config.get('excelLendName')]));
 })
 
-app.listen(8090, ()=>{
-
+app.listen(8090, async()=>{
+    console.log(new Date());
+    while(true){
+        await dataUpdater.updateData();
+        const pause = ms => new Promise(resolve => setTimeout(resolve, ms));
+        await pause(config.get('dataUpdateInterval'));
+        console.log(new Date());
+    }
 });
